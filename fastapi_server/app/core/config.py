66f0ffeab0114f06744480c0edee56922206f2f1
wnpot <egor.prokopchuk.90@mail.ru>
2024-05-01 19:32:52 +0300

    add logging

--- fastapi_server/app/core/config.py
+++ fastapi_server/app/core/config.py
@@ -1,8 +1,5 @@
-from os import getenv
-from pydantic_settings import BaseSettings
+from app.core.app_settings import AppSettings
 
 
-class Setting(BaseSettings):
-    db_url: str = 'postgresql+asyncpg://user:password@host:port/dbname[?key=value&key=value...]'
-
-settings = Setting()
+def get_app_settings() -> AppSettings:
+    return AppSettings()
--- fastapi_server/app/main.py
+++ fastapi_server/app/main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
 
+#from app.api.routes.router import router as api_router
+from app.core.config import get_app_settings
+#from app.db.db_session import initialize_database
 
-def create_app():
+
+def create_app() -> FastAPI:
+    """Instanciating and setting up FastAPI application."""
+    settings = get_app_settings()
+
+    app = FastAPI(**settings.fastapi_kwargs)
+
+    return app
+
+'''def create_app():
     app = FastAPI(
         debug=True,
         docs_url='/api/docs',
         title='FastApi Example'
     )
-    return app
+    return app'''
